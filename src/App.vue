5054bb8869816ce04e53b2edaf31be26f37de21e
Luis Cruz Sanchez <luiscrruzsanch3837@gmail.com>
2024-03-28 20:53:35 -0600

    Archivos base del proyecto + componente de formulario

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Formulario from './components/formulario.vue'
+</script>
+
+<template>
+  <h1 class="titulo">Buscador de clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario />
+
+  </div>
+</template>
